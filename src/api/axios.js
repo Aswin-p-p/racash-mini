ac5849e3fb4cefc7a07cc5633414b0247009c811
Aswin pp <aswin@techno.academy>
2025-12-16 16:31:06 +0530

    aa

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://cf5ec3edfa43.ngrok-free.app/api/v1',
+  baseURL: 'https://463bdc389377.ngrok-free.app/api/v1',
   headers: { 'Content-Type': 'application/json' },
 })
 
